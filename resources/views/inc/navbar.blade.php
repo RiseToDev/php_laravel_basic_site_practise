b38000fa21e3f58d7b073a14d80f9b8f26abbd99
RiseToDev <ram14apple@gmail.com>
2018-03-02 22:21:46 +0200

    Connected to the DataBase. Done the sending messages in contact window

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,25 +1,21 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="#">Basic navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07"
-                aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
+        <a class="navbar-brand" style="color: #a29d9d">Basic navbar</a>
         <div class="collapse navbar-collapse" id="navbarsExample07">
-            <ul class="navbar-nav mr-auto">
 
-                <li class="nav-item">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item {{Request::is('/') ? 'active' : ''}}">
                     <a class="nav-link" href="/">Home</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item {{Request::is('about') ? 'active' : ''}}">
                     <a class="nav-link" href="/about">About</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item {{Request::is('contact') ? 'active' : ''}}">
                     <a class="nav-link" href="/contact">Contact</a>
                 </li>
-
             </ul>
+
+
         </div>
     </div>
 </nav>
